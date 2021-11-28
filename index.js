6b4b691299e650e62c0c310c99f8f245c43878ec
Estanip <estani.pettigrew@gmail.com>
2021-11-28 11:50:17 -0300

    deploying

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-const mongoose = require('mongoose');
-
-const username = 'estani';
-const password = 'estani';
-const dbname = 'alquilibrodb';
-
 const app = require('./app');
-
-mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.xq5za.mongodb.net/${dbname}?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
-
-const db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "Connection error: "));
-db.once("open", () => {
-    console.log("DB connected succesfully")
-});
 
 app.listen(3000, () => {
     console.log("Server listen on port 3000")
