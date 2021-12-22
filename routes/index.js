ddf5371cfe7773297f7095926fac6055993cf858
EstaniP <estani.pettigrew@gmail.com>
2021-12-22 15:44:36 -0300

    armando modelo books

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const usersRoutes = require('./users');
 const authRoutes = require('./auth');
 const bookRoutes = require('./books');
 
-router.use('/auth', authRoutes);
+router.use('/api/auth', authRoutes);
 router.use('/api/books', bookRoutes);
 
 module.exports = router;
